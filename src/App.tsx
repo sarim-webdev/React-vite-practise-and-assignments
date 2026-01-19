bf542eb8ffa9bc5960f2f53759e35f65b0f3c904
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-01-19 23:00:30 +0500

    react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Home from "./components/Practise-hook/Home";
-import Assignment from "./components/Assignmentone/Assignment";
+// import Home from "./components/Practise-hook/Home";
+// import Assignment from "./components/assignmentone/Assignment";
+import Counterapp from "./components/counter-app/CounterApp"
 const App = () => {
   return (
     <>
-      <Home />
-      <Assignment />
+      {/* <Home /> */}
+      {/* <Assignment /> */}
+      <Counterapp />
     </>
   );
 };
---
+++ src/components/counter-app/CounterApp.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+
+const Counterapp = () => {
+    const [addValue,setAddValue] = useState(0)
+    const [subValue,setSubValue] = useState(0)
+    const [reachedValue,setReachedValue] = useState(false)
+
+    const buttonHandler = () =>{
+        setAddValue(addValue + 1)
+    }
+    const buttonHandler2 = () =>{
+        setSubValue(subValue - 1)
+    }
+
+    useEffect(() => {
+      if(addValue == 10){
+        setReachedValue(true)
+        setAddValue(0)
+        setSubValue(0)
+      }
+    },[addValue])
+  return (
+    <div>
+    { reachedValue && <div>Value is reached</div>}
+    <button onClick={buttonHandler}  style={{marginLeft:"1rem"}}>Add Values {addValue}</button>
+    <button onClick={buttonHandler2} style={{marginLeft:"1rem"}}>Sub Values {subValue}</button>
+    </div>
+  )
+}
+export default Counterapp
